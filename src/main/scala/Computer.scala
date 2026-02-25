63d75b4edda2bba297012ad6abcf72b438d51eca
marcoFratta <frattarola.marco2000@gmail.com>
2026-02-25 18:58:30 +0100

    feat: add basic computer definition

---
+++ src/main/scala/Computer.scala
@@ -0,0 +1,10 @@
+trait Bits extends (()=>Int)
+
+
+object Computer:
+  
+  case class State(x: Int, y: Int, z:Int)
+  
+
+  def compute(v:List[String]): List[Int] = ???
+
---
+++ src/test/scala/ComputerTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ComputerTest extends AnyFlatSpec with Matchers:
+  "A computer object" should "exists" in {
+
+  }
