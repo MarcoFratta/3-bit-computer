01bc2583507c847adb2c485d456fd7aa67be7409
MarcoFratta <marco.frattarola@studio.unibo.it>
2026-02-26 20:22:19 +0100

    refactor: better code design

--- src/main/scala/SingleOperandInstructions.scala
+++ src/main/scala/SingleOperandInstructions.scala
@@ -13,42 +13,37 @@ trait IpIncrement[T](step: Int) extends Instruction[T]:
 abstract class BasicOperation[T](f: (State, T) => (State, Option[T])) extends Instruction[T]:
   override def apply(s: State, operand: T): (State, Option[T]) = f(s, operand)
 
-object Operations:
+object SingleOperandInstructions:
   // IP incrementation (by default) depends on the number of operands needed by the instruction
   private val OPERANDS_NUMBER = 2
-  opaque type Instruction = BasicOperation[Int]
 
-  class Xdv extends Instruction((s, operand) =>
+  sealed case class SingleOpInstruction[T](f: (State, T) => (State, Option[T]))
+    extends BasicOperation[T](f)
+    with IpIncrement[T](OPERANDS_NUMBER)
+  
+  class Xdv extends SingleOpInstruction[Int]((s, operand) =>
     s.copy(x = (s.x / pow(2, combo(operand).get(s))).intValue) -> None)
-    with IpIncrement[Int](OPERANDS_NUMBER) 
 
-  class Yxl extends Instruction((s, operand) =>
+  class Yxl extends SingleOpInstruction[Int]((s, operand) =>
     s.copy(y = s.y ^ literal(operand).get(s)) -> None)
-    with IpIncrement[Int](OPERANDS_NUMBER) 
 
-  class Yst(bits: Int) extends Instruction((s, operand) =>
+  class Yst(bits: Int) extends SingleOpInstruction[Int]((s, operand) =>
     s.copy(y = combo(operand).get(s) % pow(2, bits).intValue) -> None)
-    with IpIncrement[Int](OPERANDS_NUMBER)
 
-  class Jnz extends Instruction((s, operand) => s.x match
+  class Jnz extends BasicOperation[Int]((s, operand) => s.x match
     case 0 => s.copy(ip = s.ip + 2) -> None
     case _ => s.copy(ip = literal(operand).get(s)) -> None)
-  
-  class Yxz extends Instruction((s, operand) =>
-    s.copy(y = s.y ^ s.z) -> None)
-    with IpIncrement[Int](OPERANDS_NUMBER) 
 
-  class Out(bits: Int) extends Instruction((s, operand) =>
+  class Yxz extends SingleOpInstruction[Int]((s, operand) => s.copy(y = s.y ^ s.z) -> None)
+
+  class Out(bits: Int) extends SingleOpInstruction[Int]((s, operand) =>
     s -> Some(combo(operand).get(s) % pow(2, bits).intValue))
-    with IpIncrement[Int](OPERANDS_NUMBER) 
 
-  class Ydv extends Instruction((s, operand) =>
+  class Ydv extends SingleOpInstruction[Int]((s, operand) =>
     s.copy(y = new Xdv().apply(s, operand)._1.x) -> None)
-    with IpIncrement[Int](OPERANDS_NUMBER) 
 
-  class Zdv extends Instruction((s, operand) =>
+  class Zdv extends SingleOpInstruction[Int]((s, operand) =>
     s.copy(z = new Xdv().apply(s, operand)._1.x) -> None)
-    with IpIncrement[Int](OPERANDS_NUMBER) 
 
 
 
--- src/test/scala/InstructionsTest.scala
+++ src/test/scala/InstructionsTest.scala
@@ -1,5 +1,5 @@
 import Computer.State
-import Operations.*
+import SingleOperandInstructions.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
