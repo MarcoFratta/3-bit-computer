99625f94baf1ec07f41d7558cc1b40551d35794f
MarcoFratta <marco.frattarola@studio.unibo.it>
2026-02-26 18:51:21 +0100

    feat: handle ip in instructions

--- src/main/scala/Instructions.scala
+++ src/main/scala/Instructions.scala
@@ -4,63 +4,58 @@ import Operand.{combo, literal}
 import scala.math.pow
 
 trait Instruction[T]:
-  val opcode: Int
+  val opCode: Int
 
   def apply(s: State, operand: T): (State, Option[T])
 
+trait OpCode[T](oc: Int) extends Instruction[T]:
+  override val opCode: Int = oc
+
+trait IpIncrement[T](step: Int) extends Instruction[T]:
+  abstract override def apply(s: State, operand: T): (State, Option[T]) =
+    val (s2, o) = super.apply(s, operand)
+    s2.copy(ip = s2.ip + step) -> o
+
+abstract class BasicOperation[T](f: (State, T) => (State, Option[T])) extends Instruction[T]:
+  override def apply(s: State, operand: T): (State, Option[T]) = f(s, operand)
+
 object Operations:
+  opaque type Instruction = BasicOperation[Int]
 
-  class Xdv extends Instruction[Int]:
-    override val opcode: Int = 0
+  class Xdv extends Instruction((s, operand) =>
+    s.copy(x = (s.x / pow(2, combo(operand).get(s))).intValue()) -> Option.empty)
+    with IpIncrement[Int](2) with OpCode[Int](0)
 
-    def apply(s: State, operand: Int): (State, Option[Int]) =
-      s.copy(x = (s.x / pow(2, combo(operand).get(s))).intValue()) -> Option.empty
+  class Yxl extends Instruction((s, operand) =>
+    s.copy(y = s.y ^ literal(operand).get(s)) -> Option.empty)
+    with IpIncrement[Int](2) with OpCode[Int](1)
+
+  class Yst extends Instruction((s, operand) =>
+    s.copy(y = combo(operand).get(s) % 8) -> Option.empty)
+    with IpIncrement[Int](2) with OpCode[Int](2)
+
+  class Jnz extends Instruction((s, operand) => s.x match
+    case 0 => s.copy(ip = s.ip + 2) -> Option.empty
+    case _ => s.copy(ip = literal(operand).get(s)) -> Option.empty)
+    with OpCode[Int](3)
+
+  class Yxz extends Instruction((s, operand) =>
+    s.copy(y = s.y ^ s.z) -> Option.empty)
+    with IpIncrement[Int](2) with OpCode[Int](4)
+
+  class Out extends Instruction((s, operand) =>
+    s -> Option(combo(operand).get(s) % 8))
+    with IpIncrement[Int](2) with OpCode[Int](5)
+
+  class Ydv extends Instruction((s, operand) =>
+    s.copy(y = new Xdv().apply(s, operand)._1.x) -> Option.empty)
+    with IpIncrement[Int](2) with OpCode[Int](6)
+
+  class Zdv extends Instruction((s, operand) =>
+    s.copy(z = new Xdv().apply(s, operand)._1.x) -> Option.empty)
+    with IpIncrement[Int](2) with OpCode[Int](7)
 
 
-  class Yxl extends Instruction[Int]:
-    override val opcode: Int = 1
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) =
-      s.copy(y = s.y ^ literal(operand).get(s)) -> Option.empty
-
-  class Yst extends Instruction[Int]:
-    override val opcode: Int = 2
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) =
-      s.copy(y = combo(operand).get(s) % 8) -> Option.empty
-
-  class Jnz extends Instruction[Int]:
-    override val opcode: Int = 3
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) = s.x match
-      case 0 => s -> Option.empty
-      case _ => s.copy(ip = literal(operand).get(s)) -> Option.empty
-
-  class Yxz extends Instruction[Int]:
-    override val opcode: Int = 4
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) =
-      s.copy(y = s.y ^ s.z) -> Option.empty
-
-  class Out extends Instruction[Int]:
-    override val opcode: Int = 5
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) =
-      s -> Option(combo(operand).get(s) % 8)
-
-  class Ydv extends Instruction[Int]:
-    override val opcode: Int = 6
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) =
-      val op = Xdv()
-      s.copy(y = op.apply(s, operand)._1.x) -> Option.empty
-
-  class Zdv extends Instruction[Int]:
-    override val opcode: Int = 7
-
-    override def apply(s: State, operand: Int): (State, Option[Int]) =
-      val op = Xdv()
-      s.copy(z = op.apply(s, operand)._1.x) -> Option.empty
 
 
 
--- src/test/scala/InstructionsTest.scala
+++ src/test/scala/InstructionsTest.scala
@@ -5,12 +5,13 @@ import org.scalatest.matchers.should.Matchers
 
 class InstructionsTest extends AnyFlatSpec with Matchers:
 
-  // OPCODE 0
+  // opCode 0
   "xdv(x = 10, operand = 2) " should "set x = 10/4 = 2.5 = 2" in {
     val instruction = Xdv()
-    instruction.opcode shouldBe 0
+    instruction.opCode shouldBe 0
     val s = State(0,10,0,0)
     instruction(s, 2)._1.x shouldBe 2
+    instruction(s, 2)._1.ip shouldBe 2
   }
   "xdv(x = 1024, operand = 6 (z = 9) " should "set x = 1024/2^9 = 2" in {
     val instruction = Xdv()
@@ -18,10 +19,10 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
     instruction(s, 6)._1.x shouldBe 2
   }
 
-  // OPCODE 1
+  // opCode 1
   "yxl(y = 2, operand = 5)" should "set y = 7" in {
     val instruction = Yxl()
-    instruction.opcode shouldBe 1
+    instruction.opCode shouldBe 1
     val s = State(0, 0, 2, 0)
     instruction(s, 5)._1.y shouldBe 7
   }
@@ -32,10 +33,10 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
     instruction(s, 0)._1.y shouldBe 0
   }
 
-  // OPCODE 2
+  // opCode 2
   "yst(operand = 6 -> (z=8))" should "set y = 0 (8 % 8 = 0)" in {
     val instruction = Yst()
-    instruction.opcode shouldBe 2
+    instruction.opCode shouldBe 2
     val s = State(0, 0, 0, 8)
     instruction(s, 6)._1.y shouldBe 0
   }
@@ -45,12 +46,12 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
     val s = State(0, 0, 0, 0)
     instruction(s, 3)._1.y shouldBe 3
   }
-  // OPCODE 3
-  "jnz with x = 0" should "do nothing" in {
+  // opCode 3
+  "jnz with x = 0" should "not jump" in {
     val instruction = Jnz()
-    instruction.opcode shouldBe 3
+    instruction.opCode shouldBe 3
     val s = State(0, 0, 0, 0)
-    instruction(s, 5)._1 shouldEqual s
+    instruction(s, 5)._1 should not be 5
   }
 
   "jnz(operand=5) with x = 12" should "jump ip to 5" in {
@@ -59,44 +60,58 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
     instruction(s, 5)._1.ip shouldEqual 5
   }
 
-  // OPCODE 4
+  // opCode 4
   "yxz(y = 3, z = 1 ) " should "set y = 2" in {
 //    y =   0 1 1
 //    z =   0 0 1
 //    xor = 0 1 0 = 2
     val instruction = Yxz()
-    instruction.opcode shouldBe 4
+    instruction.opCode shouldBe 4
     val s = State(0, 0, 3, 1)
     instruction(s, 30)._1.y shouldEqual 2
   }
-  // OPCODE 5
+  // opCode 5
   "out(operand = 3)" should "output 3" in {
     val instruction = Out()
-    instruction.opcode shouldBe 5
+    instruction.opCode shouldBe 5
     val s = State(0, 0, 3, 1)
     instruction(s, 3)._2 shouldEqual Option(3)
   }
 
   "out(operand = 6) z = 24" should "output 0" in {
     val instruction = Out()
-    instruction.opcode shouldBe 5
+    instruction.opCode shouldBe 5
     val s = State(0, 0, 0, 24)
     instruction(s, 6)._2 shouldEqual Option(0)
   }
 
-  // OPCODE 6
+  // opCode 6
   "xdv(x = 16, operand = 3) " should "set y = 16/8 = 2" in {
     val instruction = Ydv()
-    instruction.opcode shouldBe 6
+    instruction.opCode shouldBe 6
     val s = State(0, 16, 0, 0)
     instruction(s, 3)._1.y shouldBe 2
   }
 
-  // OPCODE 7
+  // opCode 7
   "xdv(x = 16, operand = 3) " should "set z = 16/8 = 2" in {
     val instruction = Zdv()
-    instruction.opcode shouldBe 7
+    instruction.opCode shouldBe 7
     val s = State(0, 16, 0, 0)
     instruction(s, 3)._1.z shouldBe 2
   }
+
+   "all instructions except jnz" should "increase ip by 2" in {
+     val instructions = List(Xdv(), Yxl(), Yst(), Yxz(), Out(),
+       Ydv(), Zdv())
+     val s = State(0,0,0,0)
+     instructions foreach (
+       f => f(s, 0)._1.ip shouldBe 2
+     )
+   }
+   "jnz, when x = 0" should "increase by 2 the ip" in {
+     val instruction = Jnz()
+     val s = State(0,0,0,0)
+     instruction(s, 2)._1.ip shouldBe 2
+   }
 
