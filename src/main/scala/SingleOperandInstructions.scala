a6e77e450fa6e2f6bfadde53473444a95e25735b
MarcoFratta <marco.frattarola@studio.unibo.it>
2026-02-27 09:48:08 +0100

    refactor: make instructions not depend on custom operand type

--- src/main/scala/SingleOperandInstructions.scala
+++ src/main/scala/SingleOperandInstructions.scala
@@ -1,5 +1,5 @@
 import Computer.State
-import Operand.{combo, literal}
+import IntOperands.OperandPolicy
 
 import scala.math.pow
 
@@ -15,35 +15,35 @@ abstract class BasicOperation[T](f: (State, T) => (State, Option[T])) extends In
 
 object SingleOperandInstructions:
   // IP incrementation (by default) depends on the number of operands needed by the instruction
-  private val OPERANDS_NUMBER = 2
-
+  private val IP_STEP_NUMBER = 2
+  
   sealed case class SingleOpInstruction[T](f: (State, T) => (State, Option[T]))
     extends BasicOperation[T](f)
-    with IpIncrement[T](OPERANDS_NUMBER)
+    with IpIncrement[T](IP_STEP_NUMBER)
   
-  class Xdv extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(x = (s.x / pow(2, combo(operand).get(s))).intValue) -> None)
+  class Xdv(getter: OperandPolicy) extends SingleOpInstruction[Int]((s, operand) =>
+    s.copy(x = (s.x / pow(2, getter(operand)(s))).intValue) -> None)
 
-  class Yxl extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(y = s.y ^ literal(operand).get(s)) -> None)
+  class Yxl(getter: OperandPolicy) extends SingleOpInstruction[Int]((s, operand) =>
+    s.copy(y = s.y ^ getter(operand)(s)) -> None)
 
-  class Yst(bits: Int) extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(y = combo(operand).get(s) % pow(2, bits).intValue) -> None)
+  class Yst(bits: Int, getter: OperandPolicy) extends SingleOpInstruction[Int]((s, operand) =>
+    s.copy(y = getter(operand)(s) % pow(2, bits).intValue) -> None)
 
-  class Jnz extends BasicOperation[Int]((s, operand) => s.x match
-    case 0 => s.copy(ip = s.ip + 2) -> None
-    case _ => s.copy(ip = literal(operand).get(s)) -> None)
+  class Jnz(getter: OperandPolicy) extends BasicOperation[Int]((s, operand) => s.x match
+    case 0 => s.copy(ip = s.ip + IP_STEP_NUMBER) -> None
+    case _ => s.copy(ip = getter(operand)(s)) -> None)
 
   class Yxz extends SingleOpInstruction[Int]((s, operand) => s.copy(y = s.y ^ s.z) -> None)
 
-  class Out(bits: Int) extends SingleOpInstruction[Int]((s, operand) =>
-    s -> Some(combo(operand).get(s) % pow(2, bits).intValue))
+  class Out(bits: Int, getter: OperandPolicy) extends SingleOpInstruction[Int]((s, operand) =>
+    s -> Some(getter(operand)(s) % pow(2, bits).intValue))
 
-  class Ydv extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(y = new Xdv().apply(s, operand)._1.x) -> None)
+  class Ydv(getter: OperandPolicy) extends SingleOpInstruction[Int]((s, operand) =>
+    s.copy(y = new Xdv(getter).apply(s, operand)._1.x) -> None)
 
-  class Zdv extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(z = new Xdv().apply(s, operand)._1.x) -> None)
+  class Zdv(getter: OperandPolicy) extends SingleOpInstruction[Int]((s, operand) =>
+    s.copy(z = new Xdv(getter).apply(s, operand)._1.x) -> None)
 
 
 
