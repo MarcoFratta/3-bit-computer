8d8f9884af5e9dc339503d161f4765301d0de6cf
MarcoFratta <marco.frattarola@studio.unibo.it>
2026-02-28 15:53:59 +0100

    feat: make intructions handle errors

--- src/main/scala/SingleOperandInstructions.scala
+++ src/main/scala/SingleOperandInstructions.scala
@@ -1,26 +1,29 @@
-import Computer.State
 import Operands.OperandPolicy
+import SingleOperandComputer.State
 
 import scala.math.pow
+import scala.util.{Failure, Success, Try}
 
-trait Instruction[T] extends ((State, T) => (State, Option[T]))
+trait Instruction[T] extends ((State, T) => Try[(State, Option[T])])
 
 trait IpIncrement[T](step: Int) extends Instruction[T]:
-  abstract override def apply(s: State, operand: T): (State, Option[T]) =
-    val (s2, o) = super.apply(s, operand)
-    s2.copy(ip = s2.ip + step) -> o
+  abstract override def apply(s: State, operand: T): Try[(State, Option[T])] =   
+    super.apply(s, operand).map((s2, o) =>  s2.copy(ip = s2.ip + step) -> o)
+   
 
 abstract class BasicOperation[T](f: (State, T) => (State, Option[T])) extends Instruction[T]:
-  override def apply(s: State, operand: T): (State, Option[T]) = f(s, operand)
+  override def apply(s: State, operand: T): Try[(State, Option[T])] = Try {
+    f(s, operand)
+  }
 
 object SingleOperandInstructions:
   // IP incrementation (by default) depends on the number of operands needed by the instruction
   private val IP_STEP_NUMBER = 2
-  
+
   sealed case class SingleOpInstruction[T](f: (State, T) => (State, Option[T]))
     extends BasicOperation[T](f)
     with IpIncrement[T](IP_STEP_NUMBER)
-  
+
   class Xdv(getter: OperandPolicy[Int]) extends SingleOpInstruction[Int]((s, operand) =>
     s.copy(x = (s.x / pow(2, getter(operand)(s))).intValue) -> None)
 
@@ -40,17 +43,7 @@ object SingleOperandInstructions:
     s -> Some(getter(operand)(s) % pow(2, bits).intValue))
 
   class Ydv(getter: OperandPolicy[Int]) extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(y = new Xdv(getter).apply(s, operand)._1.x) -> None)
+    Xdv(getter)(s, operand).map(r => s.copy(y =r._1.x) -> None).get)
 
   class Zdv(getter: OperandPolicy[Int]) extends SingleOpInstruction[Int]((s, operand) =>
-    s.copy(z = new Xdv(getter).apply(s, operand)._1.x) -> None)
-
-
-
-
-
-
-  
-  
-  
-  
+    Xdv(getter)(s, operand).map(r => s.copy(z =r._1.x) -> None).get)
--- src/test/scala/InstructionsTest.scala
+++ src/test/scala/InstructionsTest.scala
@@ -1,8 +1,10 @@
-import Computer.State
 import Operands.*
+import SingleOperandComputer.State
 import SingleOperandInstructions.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+
+import scala.util.{Failure, Success}
 
 class InstructionsTest extends AnyFlatSpec with Matchers:
 
@@ -13,53 +15,54 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
     7 -> (_ => throw IllegalArgumentException("7 is not a valid combo argument")))
   // opCode 0
   "xdv(x = 10, operand = 2) " should "set x = 10/4 = 2.5 = 2" in {
-    val instruction = Xdv(testCombo) 
+    val instruction = Xdv(testCombo)
     val s = State(0,10,0,0)
-    instruction(s, 2)._1.x shouldBe 2
-    instruction(s, 2)._1.ip shouldBe 2
+    instruction(s, 2).get._1.x shouldBe 2
+    instruction(s, 2).get._1.ip shouldBe 2
   }
   "xdv(x = 1024, operand = 6 (z = 9) " should "set x = 1024/2^9 = 2" in {
     val instruction = Xdv(testCombo)
     val s = State(0, 1024, 0, 9)
-    instruction(s, 6)._1.x shouldBe 2
+    instruction(s, 6).get._1.x shouldBe 2
   }
+
 
   // opCode 1
   "yxl(y = 2, operand = 5)" should "set y = 7" in {
     val instruction = Yxl(identity)
     val s = State(0, 0, 2, 0)
-    instruction(s, 5)._1.y shouldBe 7
+    instruction(s, 5).get._1.y shouldBe 7
   }
 
   "yxl(y = 0, operand = 0)" should "set y = 0" in {
     val instruction = Yxl(identity)
     val s = State(0, 0, 0, 0)
-    instruction(s, 0)._1.y shouldBe 0
+    instruction(s, 0).get._1.y shouldBe 0
   }
 
   // opCode 2
   "yst(operand = 6 -> (z=8))" should "set y = 0 (8 % 8 = 0)" in {
     val instruction = Yst(3, testCombo)
     val s = State(0, 0, 0, 8)
-    instruction(s, 6)._1.y shouldBe 0
+    instruction(s, 6).get._1.y shouldBe 0
   }
 
   "yst(operand = 3))" should "set y = 0 (3 % 8 = 3)" in {
     val instruction = Yst(3, testCombo)
     val s = State(0, 0, 0, 0)
-    instruction(s, 3)._1.y shouldBe 3
+    instruction(s, 3).get._1.y shouldBe 3
   }
   // opCode 3
   "jnz with x = 0" should "not jump" in {
     val instruction = Jnz(identity)
     val s = State(0, 0, 0, 0)
-    instruction(s, 5)._1 should not be 5
+    instruction(s, 5).get._1 should not be 5
   }
 
   "jnz(operand=5) with x = 12" should "jump ip to 5" in {
     val instruction = Jnz(identity)
     val s = State(1, 12, 0, 0)
-    instruction(s, 5)._1.ip shouldEqual 5
+    instruction(s, 5).get._1.ip shouldEqual 5
   }
 
   // opCode 4
@@ -69,33 +72,33 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
 //    xor = 0 1 0 = 2
     val instruction = Yxz()
     val s = State(0, 0, 3, 1)
-    instruction(s, 30)._1.y shouldEqual 2
+    instruction(s, 30).get._1.y shouldEqual 2
   }
   // opCode 5
   "out(operand = 3)" should "output 3" in {
     val instruction = Out(3, testCombo)
     val s = State(0, 0, 3, 1)
-    instruction(s, 3)._2 shouldEqual Option(3)
+    instruction(s, 3).get._2 shouldEqual Option(3)
   }
 
   "out(operand = 6) z = 24" should "output 0" in {
     val instruction = Out(3, testCombo)
     val s = State(0, 0, 0, 24)
-    instruction(s, 6)._2 shouldEqual Option(0)
+    instruction(s, 6).get._2 shouldEqual Option(0)
   }
 
   // opCode 6
   "xdv(x = 16, operand = 3) " should "set y = 16/8 = 2" in {
     val instruction = Ydv(testCombo)
     val s = State(0, 16, 0, 0)
-    instruction(s, 3)._1.y shouldBe 2
+    instruction(s, 3).get._1.y shouldBe 2
   }
 
   // opCode 7
   "xdv(x = 16, operand = 3) " should "set z = 16/8 = 2" in {
     val instruction = Zdv(testCombo)
     val s = State(0, 16, 0, 0)
-    instruction(s, 3)._1.z shouldBe 2
+    instruction(s, 3).get._1.z shouldBe 2
   }
 
    "all instructions except jnz" should "increase ip by 2" in {
@@ -103,12 +106,20 @@ class InstructionsTest extends AnyFlatSpec with Matchers:
        Ydv(testCombo), Zdv(testCombo))
      val s = State(0,0,0,0)
      instructions foreach (
-       f => f(s, 0)._1.ip shouldBe 2
+       f => f(s, 0).get._1.ip shouldBe 2
      )
    }
    "jnz, when x = 0" should "increase by 2 the ip" in {
      val instruction = Jnz(identity)
      val s = State(0,0,0,0)
-     instruction(s, 2)._1.ip shouldBe 2
+     instruction(s, 2).get._1.ip shouldBe 2
    }
+  "xdv(x = 1024, operand = 7)" should "return a failure" in {
+    val instruction = Xdv(testCombo)
+    val s = State(0, 1024, 0, 9)
+    instruction(s, 7) match {
+      case Failure(exception) =>
+      case Success(value) => fail()
+    }
+  }
 
